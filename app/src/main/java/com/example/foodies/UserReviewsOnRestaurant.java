55f598048aae785ab17be3f09052a5acb014f62c
Carmel <sombrero3@walla.com>
2022-01-11 17:20:55 +0200

    carmel branch

--- app/src/main/java/com/example/foodies/UserReviewsOnRestaurant.java
+++ app/src/main/java/com/example/foodies/UserReviewsOnRestaurant.java
@@ -3,12 +3,15 @@ package com.example.foodies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class UserReviewsOnRestaurant extends AppCompatActivity {
-
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_reviews_on_restaurant);
+        b = findViewById(R.id.button_test);
     }
+
 }
