78b7e94eb394c38d26eb7a34ee237e5645ec23e6
Carmel <sombrero3@walla.com>
2022-01-13 19:05:11 +0200

    model commit 5

--- app/src/main/java/com/example/foodies/model/Model.java
+++ app/src/main/java/com/example/foodies/model/Model.java
@@ -1,12 +1,13 @@
 package com.example.foodies.model;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Model {
 
     List<User> userList = new LinkedList<>();
-    List<Restaurant> restaurantList = new LinkedList<>();
+    List<Restaurant> restaurantList = new ArrayList<>();
     List<Dish> dishList = new LinkedList<>();
     List<Review> reviewList = new LinkedList<>();
 
@@ -44,10 +45,34 @@ public class Model {
 
     public Dish getDishById(String dishId){
         for(int i=0;i<dishList.size();i++){
-            if(dishId == dishList.get(i).id){
+            if(dishList.get(i).id.equals(dishId)){
                 return dishList.get(i);
             }
         }
         return new Dish();
+    }
+    public Restaurant getRestaurantById(String id){
+        for(int i=0;i<restaurantList.size();i++){
+            if(restaurantList.get(i).getId().equals(id)){
+                return restaurantList.get(i);
+            }
+        }
+        return new Restaurant();
+    }
+    public Review getReviewById(String id){
+        for(int i=0;i<reviewList.size();i++){
+            if(reviewList.get(i).getId().equals(id)){
+                return reviewList.get(i);
+            }
+        }
+        return new Review();
+    }
+    public User getUserById(String id){
+        for(int i=0;i< userList.size();i++){
+            if(userList.get(i).getId().equals(id)){
+                return userList.get(i);
+            }
+        }
+        return new User();
     }
 }
--- app/src/main/java/com/example/foodies/model/Restaurant.java
+++ app/src/main/java/com/example/foodies/model/Restaurant.java
@@ -1,14 +1,135 @@
 package com.example.foodies.model;
 
+import android.media.Image;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Restaurant {
     String id;
     String name;
     String location;
-    int rating;
+    Image image;
+    String rating;
+    List<String> usersVisited;
     List<Dish> dishList;
 
+    //-------Constructors-------//
+    public Restaurant(){
+        String id = Integer.toString(IdGenerator.instance.getNextId());
+        name = "";
+        location = "";
+        rating = "No rating yet";
+        dishList=new ArrayList<>();
+        usersVisited = new ArrayList<>();
+    }
+    public Restaurant(String name) {
+        String id = Integer.toString(IdGenerator.instance.getNextId());
+        this.name = name;
+        this.location = "";
+        this.rating = "";
+        dishList=new ArrayList<>();
+        usersVisited = new ArrayList<>();
+    }
+    public Restaurant(String name, String location) {
+        String id = Integer.toString(IdGenerator.instance.getNextId());
+        this.name = name;
+        this.location = location;
+        this.rating = "";
+        dishList=new ArrayList<>();
+        usersVisited = new ArrayList<>();
+    }
+    public Restaurant(String name, String location, String rating) {
+        String id = Integer.toString(IdGenerator.instance.getNextId());
+        this.name = name;
+        this.location = location;
+        this.rating = rating;
+        dishList=new ArrayList<>();
+        usersVisited = new ArrayList<>();
+    }
+    public Restaurant(String name, String location,  Dish dish) {
+        String id = Integer.toString(IdGenerator.instance.getNextId());
+        this.name = name;
+        this.location = location;
+        this.dishList = new ArrayList<>();
+        dishList.add(dish);
+        rating = dish.getRating();
+        usersVisited = new ArrayList<>();
+    }
+    public Restaurant(String name, String location,  ArrayList<Dish> dishList) {
+        String id = Integer.toString(IdGenerator.instance.getNextId());
+        this.name = name;
+        this.location = location;
+        this.dishList = dishList;
+        updateRating();
+        usersVisited = new ArrayList<>();
+    }
 
+    //-------Getters and Setters-------//
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getRating() {
+        return rating;
+    }
+    public List<Dish> getDishList() {
+        return dishList;
+    }
+    public void setDishList(List<Dish> dishList) {
+        this.dishList = dishList;
+    }
+    public List<String> getUsersVisited() {
+        return usersVisited;
+    }
+    public void setUsersVisited(List<String> usersVisited) {
+        this.usersVisited = usersVisited;
+    }
+    //    public void setRating(String rating) {
+//        this.rating = rating;
+//    }
 
+    //---------------------------------//
+
+    public void updateRating(){
+        int sum=0,avg;
+        double f ,reminder,res;
+
+        for(int i=0;i<dishList.size();i++){
+            sum+= Integer.valueOf(dishList.get(i).getRating());
+        }
+
+        f = sum/dishList.size();
+        avg = sum/dishList.size();
+        reminder = f - avg;
+        if(reminder<0.25){
+            rating = Integer.toString(sum);
+        }
+        else if(reminder>=0.25 && reminder < 0.75){
+            res = avg +0.5;
+            rating = Double.toString(res);
+        }
+        else if(reminder>=0.75){
+            rating=Integer.toString(avg+1);
+        }
+    }
+    public void addDish(Dish dish){
+        dishList.add(dish);
+        updateRating();
+    }
+    public void deleteDish(Dish dish){
+        dishList.remove(dish);
+        updateRating();
+    }
 }
