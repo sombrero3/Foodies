0a48a4f00789fcdcb7dfbeda75d11b9247dbfc0d
Or Madmoni <ormadmoni6@cs.colman.ac.il>
2022-01-15 20:57:10 +0200

    Update references

--- app/src/main/java/com/example/foodies/HomeFragment.java
+++ app/src/main/java/com/example/foodies/HomeFragment.java
@@ -11,16 +11,16 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeFragment extends Fragment{
-     Button home,myReviews,myFriends;
+     TextView home,myReviews,myFriends;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        myReviews = view.findViewById(R.id.Home_myreviews_btn);
-        myFriends = view.findViewById(R.id.Home_myfreinds_btn);
-             home = view.findViewById(R.id.Home_home_btn);
+         myReviews = view.findViewById(R.id.Home_myreviews_tv);
+         myFriends = view.findViewById(R.id.Home_myfriends_tv);
+         home = view.findViewById(R.id.Home_tv);
 
         return view;
     }
--- app/src/main/java/com/example/foodies/MyReviewsFragment.java
+++ app/src/main/java/com/example/foodies/MyReviewsFragment.java
@@ -7,18 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 
 public class MyReviewsFragment extends Fragment{
 
-    Button add,search;
+    ImageView add,search,menu,location;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_my_reviews, container, false);
-        search = view.findViewById(R.id.MyReviews_search_img_btn);
-           add = view.findViewById(R.id.MyReviews_add_img_view);
+          search = view.findViewById(R.id.MyReviews_search_img_view);
+             add = view.findViewById(R.id.MyReviews_add_img_view);
+           menu = view.findViewById(R.id.NewReview_menu_img_view);
+            location = view.findViewById(R.id.MyReviews_location_img_view);
 
         return view;
     }
