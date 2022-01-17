9fd8b2b11b376b40b896c5c39ddb2c1c446d22a6
Or Madmoni <ormadmoni6@cs.colman.ac.il>
2022-01-17 12:56:28 +0200

    Added referance

--- app/src/main/java/com/example/foodies/HomeFragment.java
+++ app/src/main/java/com/example/foodies/HomeFragment.java
@@ -12,16 +12,16 @@ import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
 
-    TextView home,myReviews,myFriends;
+   // TextView home,myReviews,myFriends;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        home = view.findViewById(R.id.Home_home_tv);
-        myReviews = view.findViewById(R.id.Home_myReviews_tv);
-        myFriends = view.findViewById(R.id.Home_myFriends_tv);
+     //   home = view.findViewById(R.id.Home_home_tv);
+     //   myReviews = view.findViewById(R.id.Home_myReviews_tv);
+    //    myFriends = view.findViewById(R.id.Home_myFriends_tv);
 
         return view;
     }
