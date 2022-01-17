35aa5728bd0aa2442faa134e0d8e405a9482558a
Carmel <sombrero3@walla.com>
2022-01-17 15:43:00 +0200

    nav repair

--- app/src/main/java/com/example/foodies/NewUserFragment.java
+++ app/src/main/java/com/example/foodies/NewUserFragment.java
@@ -8,24 +8,22 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.SectionIndexer;
+import android.widget.Button;
 
 public class NewUserFragment extends Fragment {
 
        //ImageView upload;
-     //  ImageButton signUp;
+     Button signUp;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_new_user, container, false);
-        //    ImageButton signUp =View.findViewById(R.id.NewUzer_sign_btn);
-        //   signUp.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_newUserFragment_to_homeFragment));
+        signUp = view.findViewById(R.id.NewUser_signUp_btn);
+        signUp.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_newUserFragment_to_homeFragment));
        // upload = view.findViewById(R.id.NewUser_upload_img_view);
        // signUp = view.findViewById(R.id.NewUser_sign_btn);
-
+      //  signUp.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_newUserFragment_to_homeFragment));
         return view;
     }
 }
