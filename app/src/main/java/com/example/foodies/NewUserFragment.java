1d010e30d89bd81703c3fba4b86f9f5a490cd1b5
Or Madmoni <ormadmoni6@cs.colman.ac.il>
2022-01-17 15:50:32 +0200

    Added referances

--- app/src/main/java/com/example/foodies/NewUserFragment.java
+++ app/src/main/java/com/example/foodies/NewUserFragment.java
@@ -8,27 +8,24 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SectionIndexer;
 
 public class NewUserFragment extends Fragment {
 
-       //ImageView upload;
-     //  ImageButton signUp;
+   // ImageView upload;
+  // Button signUp;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_new_user, container, false);
-        //    ImageButton signUp =View.findViewById(R.id.NewUzer_sign_btn);
-        //   signUp.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_newUserFragment_to_homeFragment));
-       // upload = view.findViewById(R.id.NewUser_upload_img_view);
-       // signUp = view.findViewById(R.id.NewUser_sign_btn);
+     // upload = view.findViewById(R.id.NewUser_upload_img_view);
+     // signUp = view.findViewById(R.id.NewUser_sign_btn);
 
         return view;
     }
 }
-
-
 
--- app/src/main/java/com/example/foodies/RegisteredUserFragment.java
+++ app/src/main/java/com/example/foodies/RegisteredUserFragment.java
@@ -7,19 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 
 public class RegisteredUserFragment extends Fragment {
 
-   //  ImageButton signIn;
+     // Button signIn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View view = inflater.inflate(R.layout.fragment_registered_user, container, false);
-      // signIn = view.findViewById(R.id.RegisteredUser_signIn_btn);
+     // signIn = view.findViewById(R.id.RegisteredUser_signIn_btn);
 
    return view;
     }
