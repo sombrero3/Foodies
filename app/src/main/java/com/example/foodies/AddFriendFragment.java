0e8e274ab8b0d6edd50fa4dedf2b3081c32e172a
Carmel <sombrero3@walla.com>
2022-01-23 17:15:36 +0200

    friend search implement

--- app/src/main/java/com/example/foodies/AddFriendFragment.java
+++ app/src/main/java/com/example/foodies/AddFriendFragment.java
@@ -23,24 +23,21 @@ import com.example.foodies.model.Restaurant;
 import com.example.foodies.model.Review;
 import com.example.foodies.model.User;
 
+import java.util.LinkedList;
 import java.util.List;
 
 
 
 public class AddFriendFragment extends Fragment {
-    List<User> friendsList;
-    TextView rvTitleTv;
+    List<User> suggestionList;
+    TextView rvTitleTv,wrongDetailsTv;
     EditText nameEt,emailEt;
     Button searchBtn;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_friend, container, false);
 
-        //implement usersRestaurantList and replace it with restaurantList---------------------------------------------------------//
-        //User user = Model.instance.getUserById(UserReviewsOnRestaurantRvFragmentArgs.fromBundle(getArguments()).getUserId());
-
-        friendsList = Model.instance.peopleYouMayKnow();
-        //-------------------------------------------------------------------------------------------------------------------------//
+        suggestionList = Model.instance.peopleYouMayKnow();
 
         RecyclerView list = view.findViewById(R.id.add_friend_rv);
         list.setHasFixedSize(true);
@@ -62,9 +59,44 @@ public class AddFriendFragment extends Fragment {
         emailEt = view.findViewById(R.id.add_friend_email_et);
         rvTitleTv = view.findViewById(R.id.add_friend_rv_title_tv);
         searchBtn = view.findViewById(R.id.add_friend_serach_btn);
+        wrongDetailsTv = view.findViewById(R.id.add_friend_wrong_details_tv);
 
+        wrongDetailsTv.setVisibility(View.INVISIBLE);
 
-       // nameTv.setText(user.getFirstName()+"'s reviews on "+restaurant.getName());
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameEt.getEditableText().toString();
+                String email = emailEt.getEditableText().toString();
+                boolean flag = true;
+
+                if (name.equals("") || name.charAt(0) == ' ') {
+                    if (email.equals("") || email.charAt(0) == ' ') {
+                        wrongDetailsTv.setVisibility(View.VISIBLE);
+                        rvTitleTv.setText("No results");
+                        suggestionList = new LinkedList<>();
+                        flag=false;
+                    } else {
+                        suggestionList = Model.instance.getUsersByEmail(email);
+                    }
+                } else if (email.equals("") || email.charAt(0) == ' ') {
+                    suggestionList = Model.instance.getUsersByName(name);
+                } else {
+                    suggestionList = Model.instance.getUsersByNameAndEmail(name,email);
+                }
+
+                list.setAdapter(adapter);
+                if(flag){
+                    wrongDetailsTv.setVisibility(View.INVISIBLE);
+                    if(suggestionList.size()>0) {
+                        rvTitleTv.setText("Search result :");
+                    }else{
+                        rvTitleTv.setText("No results");
+                    }
+                }
+            }
+        });
+
 
         return view;
 
@@ -112,7 +144,7 @@ public class AddFriendFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-            User user = friendsList.get(position);
+            User user = suggestionList.get(position);
             holder.nameEt.setText(user.getFirstName()+" "+user.getLastName());
             holder.restaurantEt.setText("Visited "+ user.getTotalRestaurantsVisited() +" restaurants total");
             holder.reviewsEt.setText("Has total of " + user.getReviewList().size()+ " reviews");
@@ -120,7 +152,7 @@ public class AddFriendFragment extends Fragment {
 
         @Override
         public int getItemCount() {
-            return friendsList.size();
+            return suggestionList.size();
         }
     }
 }
